b0e018ad62e1b3efbd11b5ac5eae3bf6a796f7ad
evgsh01 <allorean@gmail.com>
2023-02-24 20:42:32 +0300

    add

---
+++ scripts/popup.js
@@ -0,0 +1,31 @@
+class Popup {
+  #handleEscUp = (evt) => {
+    if (evt.key === "Escape") {
+      this.close();
+    }
+  };
+  constructor(classPopup) {
+    this._popupElement = document.querySelector(`.${classPopup}`);
+  }
+
+  open() {
+    this._popupElement.classList.add(`active`);
+    document.addEventListener("keyup", this.#handleEscUp);
+  }
+
+  close() {
+    this._popupElement.classList.remove(`active`);
+    document.removeEventListener("keyup", this.#handleEscUp);
+  }
+
+  setEventListener() {
+    this._popupElement.addEventListener("mousedown", (evt) => {
+      if (
+        evt.target.classList.contains("popup") ||
+        !!evt.target.closest(".popup__close")
+      ) {
+        this.close();
+      }
+    });
+  }
+}
