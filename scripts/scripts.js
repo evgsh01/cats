e0d574edcf540ccff0cb2dcee173158f1324461b
evgsh01 <allorean@gmail.com>
2023-03-02 20:59:02 +0300

    add api, cookie, login-form

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,11 +1,23 @@
+import { api } from './api.js';
+import { Card } from './card.js';
+import { PopupWithImage } from './popup-image.js';
+import { Popup } from './popup.js';
+import './utils.js';
+
 const cardsContainer = document.querySelector('.cards');
 const btnOpenPopup = document.querySelector('.btn_add');
+const btnOpenPopupLogin = document.querySelector(".login");
+
 const btnPic = document.querySelector('.btn_pic');
 
 const formCatAdd = document.querySelector('#popup-form-add');
+const formLogin = document.querySelector("#popup-form-login");
+const isAuth = Cookies.get("email");
+const MAX_LIVE_STORAGE = 10;
 
 const popupAdd = new Popup('popup-add');
 const popupImage = new PopupWithImage('popup-cat-image');
+const popupLogin = new Popup("popup-login");
 
 function serializeForm(elements) {
     const formData = {};
@@ -23,38 +35,120 @@ function serializeForm(elements) {
     return formData;
 }
 
+function createCat(dataCat){
+    const newElement = new Card(dataCat, "#card-template", handleClickCatImage);
+    cardsContainer.prepend(newElement.getElement());
+}
+
 function handleFormAddCat(e) {
     e.preventDefault();
     const elementsFormCat = [...formCatAdd.elements];
     const formData = serializeForm(elementsFormCat);
-    const newElement = new Card(formData, '#card-template', handleClickCatImage);
-    cardsContainer.prepend(newElement.getElement());
-
-    popupAdd.close();
+    api.addNewCat(formData)
+    .then(function() {
+        createCat(formData);
+        updateLocalStorage(formData, {type: 'ADD_CAT'});
+        popupAdd.close();
+    })
+    .catch(function(err){
+        console.log(err);
+    })
 }
 
 function handleClickCatImage(dataSrc) {
     popupImage.open(dataSrc)
 }
 
-formCatAdd.addEventListener('submit', handleFormAddCat)
+function handleFormLogin(e){
+    e.preventDefault();
+    const elementsFormLogin = [...formLogin.elements];
+    const formData = serializeForm(elementsFormLogin);
+    Cookies.set("email", formData.email, {expires: 7});
+    btnOpenPopup.classList.remove('visually-hidden');
+    btnOpenPopupLogin.classList.add('visually-hidden');
+    popupLogin.close()
+}
 
 btnOpenPopup.addEventListener('click', (e) => {
     e.preventDefault();
     popupAdd.open()
 })
 
-cats.forEach(catData => {
-    const newElement = new Card(catData, '#card-template', handleClickCatImage);
-    cardsContainer.append(newElement.getElement());
+btnOpenPopupLogin.addEventListener("click", (e) => {
+    e.preventDefault();
+    popupLogin.open();
 });
 
-popupAdd.setEventListener();
-popupImage.setEventListener();
+function setDataRefresh(minute, key){
+    const setTime = new Date(new Date().getTime() + minute*60000)
+    localStorage.setItem(key, setTime);
+    return setTime;
+}
 
-btnPic.addEventListener('click', () => {
+function updateLocalStorage(data, action) { // {type: 'ADD_CAT'} {type: 'ALL_CATS'}
+    const oldStorage = JSON.parse(localStorage.getItem('cats'));
+  
+    switch (action.type) {
+        case 'ADD_CAT':
+            oldStorage.push(data);
+            localStorage.setItem('cats', JSON.stringify(oldStorage));
+            return;
+        case 'ALL_CATS':
+            setDataRefresh(MAX_LIVE_STORAGE, 'catsRefresh');
+            localStorage.setItem('cats',  JSON.stringify(data));
+            return;
+        case 'DELETE_CAT':
+            const newStorage = oldStorage.filter(cat => cat.id !== data.id);
+            localStorage.setItem('cats',  JSON.stringify(newStorage));
+            return;
+        /* case 'ALL_CATS':
+            const updateStorage = oldStorage.map(cat => cat.id !== data.id ? cat : data);
+            localStorage.setItem('cats',  JSON.stringify(updateStorage));
+            return; */
+    }
+}
+
+function checkLocalStorage() {
+    const localData = JSON.parse(localStorage.getItem('cats')); //null
+    const getTimeExpires = localStorage.getItem('catsRefresh');
+
+    if(localData && localData.length && new Date() < new Date(getTimeExpires)){
+    localData.forEach((catData) => {
+        createCat(catData);
+    });
+    } else {
+    api.getAllCats()
+        .then(dataCats => {
+            dataCats.forEach((catData) => {
+            createCat(catData);
+            });
+            updateLocalStorage(dataCats, {type: 'ALL_CATS'});
+        })
+        .catch(function(err){
+        console.log(err);
+        })
+    }
+}
+
+/* btnPic.addEventListener('click', () => {
     picCats.forEach(catData => {
         const newElement = new Card(catData, '#card-template', handleClickCatImage);
         cardsContainer.append(newElement.getElement());
     });
-})
+}) */
+
+formCatAdd.addEventListener('submit', handleFormAddCat)
+formLogin.addEventListener("submit", handleFormLogin);
+
+if(!isAuth) {
+    popupLogin.open();
+    btnOpenPopup.classList.add('visually-hidden');
+} else {
+    btnOpenPopupLogin.classList.add('visually-hidden');
+}
+
+popupAdd.setEventListener();
+popupImage.setEventListener();
+popupLogin.setEventListener();
+
+checkLocalStorage()
